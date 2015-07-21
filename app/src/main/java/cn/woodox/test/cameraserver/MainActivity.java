cc0d05ecad0aa2c4d731d67330b1d4db95611ecf
niuls <niuls@vip.qq.com>
2015-07-21 11:14:45 +0800

    Add a ServerSocket function,but there is a problem can't solve - "java.net.SocketException: socket failed: EACCES (Permission denied)"

--- app/src/main/java/cn/woodox/test/cameraserver/MainActivity.java
+++ app/src/main/java/cn/woodox/test/cameraserver/MainActivity.java
@@ -3,6 +3,11 @@ package cn.woodox.test.cameraserver;
 import android.app.Activity;
 import android.os.Bundle;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 
 public class MainActivity extends Activity {
 
@@ -10,5 +15,29 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		new Thread(){
+			@Override
+			public void run() {
+				startServer();
+			}
+		}.start();
+		System.out.println("started!");
+	}
+
+	private void startServer(){
+		ServerSocket ss;
+		try {
+			ss = new ServerSocket(34567);
+			while (true){
+				Socket s = ss.accept();
+				OutputStream os = s.getOutputStream();
+				os.write("服务器连接成功！\n".getBytes("utf-8"));
+				os.close();
+				s.close();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
