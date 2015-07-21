4d54a2dd57ef04cc5550b78876cd131556876fd8
niuls <niuls@vip.qq.com>
2015-07-21 10:36:39 +0800

    Init project.

---
+++ app/src/main/java/cn/woodox/test/cameraserver/MainActivity.java
@@ -0,0 +1,14 @@
+package cn.woodox.test.cameraserver;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
