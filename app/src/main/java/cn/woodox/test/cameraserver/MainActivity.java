106f5fe44986c9c47281436642c351a6d4d8bad5
niuls <niuls@vip.qq.com>
2015-08-11 18:58:09 +0800

    add location, but not finished yet.

--- app/src/main/java/cn/woodox/test/cameraserver/MainActivity.java
+++ app/src/main/java/cn/woodox/test/cameraserver/MainActivity.java
@@ -1,12 +1,16 @@
 package cn.woodox.test.cameraserver;
 
 import android.app.Activity;
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.ImageFormat;
 import android.graphics.Rect;
 import android.graphics.YuvImage;
 import android.hardware.Camera;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -32,18 +36,21 @@ import java.util.Enumeration;
 
 
 public class MainActivity extends Activity implements SurfaceHolder.Callback,
-		Camera.PreviewCallback{
+		Camera.PreviewCallback {
 	private SurfaceView mSurfaceview = null; // SurfaceView对象：(视图组件)视频显示
 	private SurfaceHolder mSurfaceHolder = null; // SurfaceHolder对象：(抽象接口)SurfaceView支持类
 	private Camera mCamera = null; // Camera对象，相机预览
 	private byte frameRaw[] = null;
-	private boolean haveData =false;
+	private boolean haveData = false;
 	private Bitmap bmp = null;
 	private ImageView ivBox;
 	private byte jpgBytes[];
 
-	private TextView tvIP,tvPort,tvLog;
+	private TextView tvIP, tvPort, tvLog;
 	private Button btnConnect;
+
+	private LocationManager locationManager;
+	private String targetAddr = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -54,15 +61,89 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback,
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON,
 				WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
+		// 获取系统LocationManager服务
+		locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+		DebugLog.w("location:"+location.toString());
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, new LocationListener() {
+			@Override
+			public void onLocationChanged(final Location location) {
+				DebugLog.w("...");
+				new Thread() {
+					@Override
+					public void run() {
+						super.run();
+						if (targetAddr == null) {
+							return;
+						}
+						try {
+							String addr[] = targetAddr.split(":");
+							Socket socket = new Socket(addr[0], Integer.parseInt(addr[1]));
+							DataOutputStream os = new DataOutputStream(socket.getOutputStream());
+							os.writeUTF("Position:\nLongitude:" + location.getLongitude() + "\nLatitude:" +
+									location.getLatitude() + "\nAltitude:" + location.getAltitude() +
+									"\nAccuracy:" + location.getAccuracy());
+							os.flush();
+							os.close();
+							socket.close();
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+					}
+				}.start();
+			}
+
+			@Override
+			public void onStatusChanged(String provider, int status, Bundle extras) {
+
+			}
+
+			@Override
+			public void onProviderEnabled(final String provider) {
+				DebugLog.w("...");
+				new Thread() {
+					@Override
+					public void run() {
+						super.run();
+						if (targetAddr == null) {
+							return;
+						}
+						try {
+							Location location = locationManager
+									.getLastKnownLocation(provider);
+							String addr[] = targetAddr.split(":");
+							Socket socket = new Socket(addr[0], Integer.parseInt(addr[1]));
+							DataOutputStream os = new DataOutputStream(socket.getOutputStream());
+							os.writeUTF("Position:\nLongitude:" + location.getLongitude() + "\nLatitude:" +
+									location.getLatitude() + "\nAltitude:" + location.getAltitude() +
+									"\nAccuracy:" + location.getAccuracy());
+							os.flush();
+							os.close();
+							socket.close();
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+					}
+				}.start();
+			}
+
+			@Override
+			public void onProviderDisabled(String provider) {
+
+			}
+		});
+
+
 		findViews();
 
-		new Thread(){
+		new Thread() {
 			@Override
 			public void run() {
 				startServer();
 			}
 		}.start();
 		System.out.println("started!");
+
 	}
 
 	@Override
@@ -74,11 +155,11 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback,
 		super.onStart();
 	}
 
-	void findViews(){
-		tvIP = (TextView)findViewById(R.id.tvIP);
-		tvPort = (TextView)findViewById(R.id.tvPort);
-		tvLog = (TextView)findViewById(R.id.tvLog);
-		btnConnect = (Button)findViewById(R.id.btnConnect);
+	void findViews() {
+		tvIP = (TextView) findViewById(R.id.tvIP);
+		tvPort = (TextView) findViewById(R.id.tvPort);
+		tvLog = (TextView) findViewById(R.id.tvLog);
+		btnConnect = (Button) findViewById(R.id.btnConnect);
 		btnConnect.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
@@ -93,10 +174,10 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback,
 		});
 
 		mSurfaceview = (SurfaceView) findViewById(R.id.camera_preview);
-		ivBox = (ImageView)findViewById(R.id.ivBox);
+		ivBox = (ImageView) findViewById(R.id.ivBox);
 	}
 
-	private void startServer(){
+	private void startServer() {
 		ServerSocket ss;
 		try {
 			ss = new ServerSocket(30000);
@@ -109,20 +190,20 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback,
 			msg.setData(bundle);
 			uiHandler.sendMessage(msg);
 
-			while (true){
+			while (true) {
 				Socket s = ss.accept();
+				DataInputStream is = new DataInputStream(s.getInputStream());
+				targetAddr = is.readUTF();
 				DataOutputStream os = new DataOutputStream(s.getOutputStream());
-				while(true) {
-					if(!haveData){
+				while (true) {
+					if (!haveData) {
 						try {
 							Thread.sleep(20);
-						}catch (Exception e){
+						} catch (Exception e) {
 							e.printStackTrace();
 						}
 						continue;
 					}
-					DebugLog.w("bytes length:" + jpgBytes.length);
-					DebugLog.e("jpgLenth:" + jpgBytes.length);
 					os.writeInt(jpgBytes.length);
 					os.write(jpgBytes);
 //				os.write("服务器连接成功！\n".getBytes("utf-8"));
@@ -137,23 +218,23 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback,
 		}
 	}
 
-	Handler uiHandler = new Handler(){
+	Handler uiHandler = new Handler() {
 		@Override
 		public void handleMessage(Message msg) {
-			if(msg.what == 0x123){
+			if (msg.what == 0x123) {
 //				byte[] ip = msg.getData().getByteArray("ip");
 				int port = msg.getData().getInt("port");
 				try {
 //					tvIP.setText(ip[0]+"."+ip[1]+"."+ip[2]+"."+ip[3]);
 					tvIP.setText(getLocalIpAddress());
-					tvPort.setText(""+port);
-				}catch (Exception e){
+					tvPort.setText("" + port);
+				} catch (Exception e) {
 					e.printStackTrace();
 					tvIP.setText("Null");
 					tvPort.setText("Null");
 				}
 			}
-			if(msg.what == 0x456){
+			if (msg.what == 0x456) {
 //				tvLog.append(msg.getData().getString("log"));
 				ivBox.setImageBitmap(bmp);
 			}
@@ -162,12 +243,12 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback,
 
 	public String getLocalIpAddress() {
 		try {
-			for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements();) {
+			for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements(); ) {
 				NetworkInterface intf = en.nextElement();
-				for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements();) {
+				for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements(); ) {
 					InetAddress inetAddress = enumIpAddr.nextElement();
 					//过滤掉回环地址和IPv6
-					if (!inetAddress.isLoopbackAddress()&& !(inetAddress instanceof Inet6Address)) {
+					if (!inetAddress.isLoopbackAddress() && !(inetAddress instanceof Inet6Address)) {
 						return inetAddress.getHostAddress().toString();
 					}
 				}
@@ -178,9 +259,9 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback,
 		return null;
 	}
 
-	void connectServer(){
-		try{
-			Socket socket = new Socket(tvIP.getText().toString(),30000);
+	void connectServer() {
+		try {
+			Socket socket = new Socket(tvIP.getText().toString(), 30000);
 
 			DataInputStream dataInput = new DataInputStream(socket.getInputStream());
 			while (true) {
@@ -190,10 +271,8 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback,
 				while (len < size) {
 					len += dataInput.read(data, len, size - len);
 				}
-//			ByteArrayOutputStream outPut = new ByteArrayOutputStream();
 				bmp = BitmapFactory.decodeByteArray(data, 0, data.length);
 				DebugLog.e("lenth:" + data.length);
-//			bmp.compress(Bitmap.CompressFormat.JPEG, 100, outPut);
 
 				Message msg = new Message();
 				msg.what = 0x456;
@@ -202,7 +281,7 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback,
 
 //			dataInput.close();
 //			socket.close();
-		}catch (IOException e){
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
@@ -213,9 +292,11 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback,
 		InitCamera();
 	}
 
-	/**初始化摄像头*/
-	private void InitCamera(){
-		try{
+	/**
+	 * 初始化摄像头
+	 */
+	private void InitCamera() {
+		try {
 			mCamera = Camera.open();
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -225,7 +306,7 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback,
 	@Override
 	protected void onPause() {
 		super.onPause();
-		try{
+		try {
 			if (mCamera != null) {
 				mCamera.setPreviewCallback(null); // ！！这个必须在前，不然退出出错
 				mCamera.stopPreview();
@@ -258,10 +339,11 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback,
 		}
 		mCamera.stopPreview();
 		mCamera.setPreviewCallback(this);
-		mCamera.setDisplayOrientation(90); //设置横行录制
+		mCamera.setDisplayOrientation(0); //设置横行录制
 		//获取摄像头参数
 		Camera.Parameters parameters = mCamera.getParameters();
 		parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_VIDEO);
+		parameters.setRotation(180);
 		mCamera.setParameters(parameters);
 
 		mCamera.startPreview();
@@ -282,11 +364,11 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback,
 	public void onPreviewFrame(byte[] data, Camera camera) {
 		ByteArrayOutputStream jpgStream = new ByteArrayOutputStream();
 		Camera.Size size = mCamera.getParameters().getPreviewSize();
-		if(haveData)
+		if (haveData)
 			return;
-		try{
-			YuvImage image = new YuvImage(data, ImageFormat.NV21,size.width,size.height,null);
-			if(image != null){
+		try {
+			YuvImage image = new YuvImage(data, ImageFormat.NV21, size.width, size.height, null);
+			if (image != null) {
 				image.compressToJpeg(new Rect(0, 0, size.width, size.height), 80, jpgStream);
 				jpgStream.flush();
 
@@ -294,10 +376,8 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback,
 
 				frameRaw = data;
 				haveData = true;
-				DebugLog.w("dataSize:" + data.length);
-				DebugLog.w("jpegSize:" + jpgBytes.length);
 			}
-		}catch (Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
